fcf1b90a1e588f43bd2fc52a37381800afedb480
Floffah <floffah@floffah.dev>
2024-08-20 14:32:01 +0100

    feat: add powered by vercel & spotify brand images

--- apps/web/src/app/(authRoutes)/layout.tsx
+++ apps/web/src/app/(authRoutes)/layout.tsx
@@ -1,3 +1,7 @@
+import poweredByVercelImage from "../../../public/images/brands/poweredByVercel.svg";
+import spotifyImage from "../../../public/images/brands/spotify.png";
+import Image from "next/image";
+
 export default function AuthRoutesLayout({ children }) {
     return (
         <div className="flex h-screen">
@@ -5,6 +9,33 @@ export default function AuthRoutesLayout({ children }) {
                 <h1 className="w-fit text-2xl font-bold">PulseShelf</h1>
 
                 {children}
+
+                <div className="mt-6 flex items-center justify-center gap-2">
+                    <a
+                        href="https://vercel.com/?utm_source=pulseshelf"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <Image
+                            src={poweredByVercelImage}
+                            alt="Powered by Vercel"
+                            width={200}
+                        />
+                    </a>
+
+                    <a
+                        href="https://spotify.com/?utm_source=pulseshelf"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <Image
+                            src={spotifyImage}
+                            alt="Spotify"
+                            width={150}
+                            placeholder="blur"
+                        />
+                    </a>
+                </div>
             </div>
             <div className="hidden flex-grow bg-blue-500 sm:block dark:bg-blue-900"></div>
         </div>
