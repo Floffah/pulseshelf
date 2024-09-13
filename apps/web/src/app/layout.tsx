1f8286b6dc80c6a0eb02a85d357d6229bd1837b8
Floffah <floffah@floffah.dev>
2024-09-13 12:10:46 +0100

    chore: use inter font v4

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import "@/app/globals.css";
 import { populateMetadata } from "@/lib/populateMetadata";
@@ -12,12 +12,12 @@ export const metadata = populateMetadata({
     description: "Document your life through music",
 });
 
-const nunito = Nunito({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className={nunito.variable}>
-            <body className={nunito.className}>
+        <html lang="en" className={inter.variable}>
+            <body className={inter.className}>
                 <APIProvider>
                     <DialogProvider>{children}</DialogProvider>
                 </APIProvider>
