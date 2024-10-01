43f55dbdfe6e28e910d063d5cc0e1b0c1edfe0b6
Floffah <floffah@floffah.dev>
2024-10-01 10:52:45 +0100

    fix: downgrade react-hook-form

--- apps/api/tests/db.d.ts
+++
@@ -1,4 +0,0 @@
-import { Client } from "@planetscale/database";
-import { schema } from "@pulseshelf/models";
-export declare const client: Client;
-export declare const db: import("drizzle-orm/planetscale-serverless").PlanetScaleDatabase<typeof schema>;
